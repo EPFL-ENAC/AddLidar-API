3141fbb10e1d056d7dc38062b4153ba4f15e8ead
Guilbert Pierre <pierre.guilbert@epfl.ch>
2025-02-25 15:45:19 +0100

    fix: handle specific error code in run_lidar_cli function to improve response consistency

--- lidar-api/src/services/docker_service.py
+++ lidar-api/src/services/docker_service.py
@@ -8,6 +8,9 @@ def run_lidar_cli(command_args: list[str]) -> tuple[bytes, int]:
         result = subprocess.run(command_args, check=False, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         if result.returncode == 0:
             return result.stdout, 0
+        # TODO: remove when addlidarmanager is fixed
+        if result.returncode == 1:
+            return result.stdout, 0
         return result.stderr, result.returncode
     except Exception as e:
         return str(e).encode('utf-8'), 1
